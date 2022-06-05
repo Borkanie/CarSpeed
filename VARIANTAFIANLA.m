13b128a730a56c237e84ea013934e7b13924db8d
Borkanie <43271944+Borkanie@users.noreply.github.com>
2022-06-05 14:50:46 +0300

    Changes speed function resolves some bugs but the big vans till makes problems... EMA is needed for speed aproximation because of poor recognition

--- VARIANTAFIANLA.m
+++ VARIANTAFIANLA.m
@@ -1,40 +1,14 @@
-clc
-clear all
-close all
 
-P1=[600;750]*0.5;
-P2=[600;750]*0.5;
-P3=[680;725]*0.5;
-% Lungime bara
-LungimeBaraPixeli = sqrt(sum([440-760; 800-700].^2));
-
-%% CITIREA VIDEO CLIPULUI/frame-uri
-% implay('trimm.mp4');
-obj=VideoReader('litere1.mp4');
-nFrames=obj.NumFrames;
-vidHeight=obj.Height;
-vidWidth=obj.Width;
-
-isCarInRectangle=false;
-N1=0;
-mov(1:nFrames)=struct('cdata',zeros(vidHeight,vidWidth,1,'uint8'),'colormap',[]);
-for k=1:nFrames
-    frame = read(obj,k);
-    gray_frame=rgb2gray(frame);
-    % gray_frame=imresize(gray_frame,0.5);
-    % gray_frame=rgb2gray(gray_frame);
-    images(:,:,k)=gray_frame;
-    %  images1(:,:,k)=frame;
-    % imshow(gray_frame);
-end
-
-%% diferenta intre frameuri
 %areea of interes where we count car speed in pixels
-interesRectangle=[100 200 200 1000];
+interesRectangle=[100 200 400 200];
+yInteresInMetri=14;
+xInteresInMetri=10;
+yRatio=yInteresInMetri/interesRectangle(4);
+xRatio=xInteresInMetri/interesRectangle(2);
 count=0;
 [lini, coloane, Nr_poze] = size (images)
 bbox = [0 0 0 0];
-cars=[car(bbox)];
+cars=[car(bbox,0)];
 for i=1:Nr_poze-5
     differenceImage =(abs(im2double(images(:,:,i))) - im2double(images(:,:,i+5)));
     level=graythresh(differenceImage);
@@ -45,8 +19,8 @@ for i=1:Nr_poze-5
     title('IMAGINE DUPA ERODARE');
     BW3 = imdilate(BW2,strel('disk',22));
     info = regionprops(BW3,'Boundingbox','Area') ;
-    figure(5)
-    clf();
+    fig=figure(5);
+    clf(fig);
     imshow(images(:,:,i+2));
     hold on
     N=1750;
@@ -56,20 +30,21 @@ for i=1:Nr_poze-5
         area=info(k).Area;
         if(BB(2)>1)
             if (area>N)
-                if isPointInBB(getCOG(BB),interesRectangle)
-                    newcar=car(BB);
-                    cars=TryAddCarToCars(cars,newcar,interesRectangle);
-                end
-                cars=tryRemoveCarToCars(cars,interesRectangle);
-
+                newcar=car(BB,i);
+                cars=TryAddCarToCars(cars,newcar,interesRectangle,i,yRatio,xRatio);                
+                
                 rectangle('Position', [BB(1),BB(2),BB(3),BB(4)],'EdgeColor','r','LineWidth',2) ;
                 % showShape('line',[320 900 580 850]*0.5,'Color','green')
             end
             % showShape('line',[380 800 780 660]*0.5,'Color','green')
-            for i=1:length(cars)
-                chr=int2str(cars(i).speed*100);
-                text(cars(i).BB(1),cars(i).BB(2),chr,'Color','green','FontSize',10);
-            end
+%             for i=1:length(cars)               
+%                 if cars(i).isStillInInteres(interesRectangle,i)
+%                     chr=int2str(cars(i).speed*100);
+%                     text(cars(i).BB(1),cars(i).BB(2),chr,'Color','green','FontSize',10);
+%                 end
+%             end
+            hold on
+            rectangle('Position',interesRectangle,'EdgeColor','b','LineWidth',2)
             hold on
             showShape('line',[440 800 760 700]*0.5,'Color','green');
             %disp(count);
@@ -82,32 +57,33 @@ end
 %la fiecare iteratie verificam iara care sunt cele posibile sa fie noua
 %pozitie a dreptunghiului vechi
 
-function [cars]=TryAddCarToCars(cars,car,interesRectangle)
-for i=1:length(cars)
-    if cars(i).isStillInInteres(interesRectangle)
-        if cars(i).isTheSameCar(car)
-            cars(i)=cars(i).move(car.BB);
-            fprintf('Speed of car %i is %i \n',[i,cars(i).speed]);
-            return;
-        end
-    end
-
-end
-cars(length(cars)+1)=car;
-fprintf('Added car %i \n',length(cars));
-end
-
-function [newcars]=tryRemoveCarToCars(cars,interesRectangle)
+function [newcars]=TryAddCarToCars(cars,car,interesRectangle,frame,yRatio,xRatio)
 indexToBeRemoved=[];
 for i=1:length(cars)
-    if ~cars(i).isStillInInteres(interesRectangle)
-        indexToBeRemoved=[indexToBeRemoved i];
-        fprintf('Car %i is out of scope \n',i);
+    if cars(i).isTheSameCar(car)
+        if car.isStillInInteres(interesRectangle,frame)
+            cars(i)=cars(i).move(car.BB,frame,yRatio,xRatio);
+            chr=int2str(cars(i).speed*100);
+            text(cars(i).BB(1),cars(i).BB(2),chr,'Color','green','FontSize',20);
+            %fprintf('Speed of car %i is %i \n',[i,cars(i).speed]);
+            newcars=cars;
+            return;
+        else
+            indexToBeRemoved=[indexToBeRemoved i];
+            fprintf('Car %i is out of scope \n',i);
+            cars(i).BB=[0 0 0 0];
+            car.BB=[0 0 0 0];
+        end
     end
+end
+
+if car.isStillInInteres(interesRectangle,frame)
+    cars(length(cars)+1)=car;
+    fprintf('Added car %i \n',length(cars));
 end
 cars(indexToBeRemoved)=[];
 newcars=cars;
-if length(indexToBeRemoved)>1
+if length(indexToBeRemoved)>0
     fprintf('Removed %i cars \n',length(indexToBeRemoved));
 end
 end
--- car.m
+++ car.m
@@ -5,28 +5,42 @@ classdef car
     properties
         BB;
         speed=0;
+        lastFrame=0;
     end
 
     methods
-        function obj = car(BB)
+        function obj = car(BB,frame)
             %UNTITLED2 Construct an instance of this class
             %   Detailed explanation goes here
             obj.BB = BB;
             obj.speed=0;
+            obj.lastFrame=frame;
         end
         
         function [isTheSame]=isTheSameCar(obj,car)
-             isTheSame=isPointInBB(getCOG(car.BB),obj.BB);
+            isTheSame=isPointInBB(getCOG(car.BB),obj.BB);
         end
-        function [isStillRelevant]=isStillInInteres(obj,interesRectangle)
-             isStillRelevant=isPointInBB(getCOG(obj.BB),interesRectangle);
+        function [isStillRelevant]=isStillInInteres(obj,interesRectangle,frame)             
+            if obj.lastFrame-frame<50
+                isStillRelevant=isPointInBB(getCOG(obj.BB),interesRectangle);
+            else
+                isStillRelevant=0;
+            end
         end
-        function obj=move(obj,BB)
-            newcog=getCOG(BB);
-            cog=getCOG(obj.BB);
-            sp=sqrt(sum((newcog-cog).^2))/24;
-            obj.speed=sp;
+        function obj=move(obj,BB,frame,yRatio,xRatio)
+            framesMoved=frame-obj.lastFrame;%frames between locations            
+            newcog=getCOG(BB);%get new location
+            cog=getCOG(obj.BB);%get old location
+            pixelDiff=(newcog-cog);%make location difference in pixels
+            meterDiff=[pixelDiff(1)*xRatio;pixelDiff(2)*yRatio];%lienar transformatio to meters
+            meterDistance=sqrt(sum(meterDiff.^2))/24;%distance in meters as vector from Pitagora
+            recordingFrequency=24;%frames/second
+            timeInterval=framesMoved/recordingFrequency;%seconds
+            meterSpeed=meterDistance/timeInterval;
+
+            obj.speed=meterSpeed;
             obj.BB=BB;
+            obj.lastFrame=frame;%we move location
         end
     end
 end
